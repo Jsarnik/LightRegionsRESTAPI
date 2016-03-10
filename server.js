e9888eddc21cebad4b8ec98b898f291ea8d28e31
Justin Sarnik <Sarnikdev@gmail.com>
2016-03-10 09:32:34 -0800

    create SSL cert setup HTTPS

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // Get the packages we need
 var express = require('express');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var categoryController = require('./controllers/CategoryController');
@@ -10,15 +13,16 @@ var clientController = require('./controllers/client');
 var ejs = require('ejs');
 var session = require('express-session');
 var oauth2Controller = require('./controllers/oauth2');
-
-// Connect to the lightregions MongoDB
-//mongoose.connect('mongodb://localhost:27017/lightregions');
-
-// Connect to the lightregions MongoDB on mLab [Heroku]
-mongoose.connect('mongodb://db_user:password@ds045714.mlab.com:45714/heroku_85pg7kjb');
-
+var privateKey = fs.readFileSync('./ssl/key.pem', 'utf8');
+//Set our SSL Credentials
+var certificate = fs.readFileSync('./ssl/cert.pem', 'utf8');
+var credentials = {key:privateKey, cert: certificate};
 // Create our Express application
 var app = express();
+// Connect to the lightregions MongoDB
+//mongoose.connect('mongodb://localhost:27017/lightregions');
+// Connect to the lightregions MongoDB on mLab [Heroku]
+mongoose.connect('mongodb://db_user:password@ds045714.mlab.com:45714/heroku_85pg7kjb');
 
 // Set view engine to ejs
 app.set('view engine', 'ejs');
@@ -72,7 +76,15 @@ router.route('/oauth2/token')
 // Register all our routes with /api
 app.use('/api', router);
 
+
+//create http and https servers
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
 // Use environment defined port or 3000
-var port = process.env.PORT || 3000;
+var httpPort = process.env.PORT || 3000;
+var httpsPort = process.env.PORT || 8000;
+
 // Start the server
-app.listen(port);
+//httpServer.listen(httpPort);
+httpsServer.listen(httpsPort);
